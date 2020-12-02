371484c1976949634e6d84ca7a050129d6ef6e38
Brandon Piercy <b.piercy567@gmail.com>
2020-12-01 20:48:29 -0800

    created the employee class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,24 @@
-// TODO: Write code to define and export the Employee class
+//Class to be exported which provides the basis for every employee generation
+class Employee {
+    constructor (name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email=email;
+    }
+
+    getName() {
+        console.log(this.name);
+    }
+
+    getId() {
+        console.log(this.id);
+    }
+
+    getEmail() {
+        console.log(this.email);
+    }
+
+    getRole() {
+        console.log("employee")
+    }
+}
