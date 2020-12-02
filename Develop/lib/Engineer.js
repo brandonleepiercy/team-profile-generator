59f3c36ea36a4f98a0512f85f1e7a1fc82dd543d
Brandon Piercy <b.piercy567@gmail.com>
2020-12-01 21:10:44 -0800

    added comments

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,5 +1,7 @@
+//Importing the methods and properties from the Employee Class
 const Employee = require("./Employee");
 
+//Extending the employee class with the Engineer Class
 class Engineer extends Employee {
     constructor (name, id, email, github) {
         super(name,id,email);
